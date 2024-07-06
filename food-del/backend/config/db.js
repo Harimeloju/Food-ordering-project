25b36b6cf5ff9eba05c8fcf13de11f2acd0d597d
haridharshan <haridharshanmeloju1820@gmail.com>
2024-07-07 04:07:21 +0530

    firstcommit

--- food-del/backend/config/db.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
- export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://pallavi:hari@cluster0.p0ob9ga.mongodb.net/food-del').then(()=>console.log("DB connected"));
-}
